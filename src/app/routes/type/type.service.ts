80c446bd68f208d2fccce81d08df5672e3bac0c1
howco-dvdreedy <david.reedy@howcogroup.com>
2024-01-22 11:50:47 -0600

    fix mistake with type

--- src/app/routes/type/type.service.ts
+++ src/app/routes/type/type.service.ts
@@ -4,23 +4,20 @@ import cache from 'memory-cache';
 import { CACHE_KEYS } from '../../constants/cacheKeys';
 
 const getType = async (): Promise<Type[]> => {
+  const cachedResponse = cache.get<Type[]>(CACHE_KEYS.TYPES);
+  if (cachedResponse) {
+    return cachedResponse;
+  }
 
-    const cachedResponse = cache.get<Type[]>(CACHE_KEYS.TYPES);
-    if (cachedResponse) {
-      return cachedResponse;
-    }
-
-  const user = process.env.DB_USER
-  const password = process.env.DB_PASSWORD
-  const connString = `Driver={SQL Server};Server=s-cu-hessql.howcogroup.com;Database=howco_dw_live;Trusted_Connection=Yes;UID=${user};PWD=${password};`
+  const user = process.env.DB_USER;
+  const password = process.env.DB_PASSWORD;
+  const connString = `Driver={SQL Server};Server=s-cu-hessql.howcogroup.com;Database=howco_dw_live;Trusted_Connection=Yes;UID=${user};PWD=${password};`;
   const connection = await odbc.connect(connString);
   const sqlQueryForType = `SELECT poptype_id as id, poptype_description as description from poptype`;
-		const type =  await connection.query<Type>(
-			sqlQueryForType
-		);
-		return type;
-};
+  const type = await connection.query<Type>(sqlQueryForType);
+  cache.put(CACHE_KEYS.TYPES, type, 1000 * 60 * 60 * 24);
 
-cache.put(CACHE_KEYS.TYPES, getType, 1000 * 60 * 60 * 24);
+  return type;
+};
 
 export default getType;
